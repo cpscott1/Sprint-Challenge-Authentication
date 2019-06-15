db00f391535200205d8a4d2bc6f1590d327d341b
cpscott1 <thacam18@gmail.com>
2019-06-15 11:56:41 -0500

    initial setup

--- config/routes.js
+++ config/routes.js
@@ -8,12 +8,51 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    roles: ['users']
+  }
+
+  const options = {
+    expiresIn: '1d'
+  }
+
+  return jwt.sign(payload, process.env.JWT_SECRET, options)
+}
+
 function register(req, res) {
-  // implement user registration
+  let user = req.body;
+
+  const hash = bcrypt.hashSync(user.password, 10);
+
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
 }
 
 function login(req, res) {
-  // implement user login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user)
+        res.status(200).json({ message: `You are now entering the twilight zone ${user.username}`,
+          token
+        })
+      } else {
+        res.status(401).json({ message: 'Credentials no good!!!' })
+      }
+    })
 }
 
 function getJokes(req, res) {
